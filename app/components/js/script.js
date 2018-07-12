bf828e6cf5d68f39c54f617f2c1796ab67d3c924
trungdq <trungdangquang@admicro.vn>
2018-07-12 17:34:32 +0700

    update

--- app/components/js/script.js
+++ app/components/js/script.js
@@ -1,3 +1,84 @@
+
+var App = (function () {
+    var self = this;
+    this.module_name = 'App';
+    var animate, scale;
+    
+    animate = (function (_this) {
+        return function (element, delay) {
+            var func;
+            if(element) {
+                func = function (entries) {
+                    return entries.forEach(function (e) {
+                        if(e.intersectionRatio > 0.9) {
+                           return setTimeout(function () {
+                               return $(element).addClass('animate');
+                           }, delay);
+                        }
+                    });
+                };
+                return (new IntersectionObserver(func, {
+                    rootMargin: '0px',
+                    threshold: 1.0
+                })).observe(element);
+            }
+        };
+    })(this);
+
+$((function (_this) {
+    return function () {
+        if($('html').width() >= 544) {
+            $(window).load(function () {
+                return window.requestAnimationFrame(function () {
+                    return setTimeout(function () {
+                        return $('.section_welcome .iphone').addClass('animate');
+                    }, 200);
+                });
+            });
+        }else {
+            $('.section_welcome .iphone .phone').css('transition', 'none')
+                .css('opacity', 1).css('transform', 'translateY(0) translateZ(1px)');
+        }
+    };
+})(this));
+
+scale = (function (_this) {
+    return function () {
+        var s,w;
+        w = $('html').width();
+        if(w < 1024) {
+            if (w < 544) {
+                s = w / 320;
+                return $('.scale').css('transform', 'scale(' + s + ')');
+            } else {
+                s = w / 1024;
+                return $('.scale').css('transform', 'scale(' + s + ')');
+            }
+        } else {
+            return $('.scale').css('transform', '');
+        }
+    }
+})(this);
+
+$(window).resize((function (_this) {
+    return function () {
+      return scale();
+    };
+})(this));
+
+$((function (_this) {
+    return function () {
+        return scale();
+    }
+})(this));
+
+
+;return this}).call(App || {});
+
+
+
+
+
 // create the module and name it scotchApp
 var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
